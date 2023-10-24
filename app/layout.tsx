008fef1727d63494e43c2358faa7ff7135350388
Beast51 <7007500@gmail.com>
2023-10-24 05:54:16 +0300

    fix

--- app/layout.tsx
+++ app/layout.tsx
@@ -45,10 +45,10 @@ export const metadata: Metadata = {
       color: '#fff',
       media: '(prefers-color-scheme: light)',
     },
-    {
-      color: '#1f1f1f',
-      media: '(prefers-color-scheme: dark)',
-    },
+    // {
+    //   color: '#1f1f1f',
+    //   media: '(prefers-color-scheme: dark)',
+    // },
   ],
 };
 
